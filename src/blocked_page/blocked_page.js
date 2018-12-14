5ff15d2a820650b8bb931d9cc06e57eebd259df5
Aesthetic Byte <aestheticbyte@gmail.com>
2018-12-14 17:09:35 -0500

    Allow the user to go back to previous url on blocked page

--- src/blocked_page/blocked_page.js
+++ src/blocked_page/blocked_page.js
@@ -3,13 +3,16 @@ window.onload = function() {
 	var go_back_link = document.getElementById("go-back");
 	var unblacklist_link = document.getElementById("unblacklist")
 
+	// If the user wants to go back after giving themselves more time, look into the history
+	// of the current tab and go back by one.
 	go_back_link.onclick = function() {
-		console.log("hullu!");
+		window.history.back();
 		return false;
 	}
 
 	unblacklist_link.onclick = function() {
-		console.log("hollo!");
+		var tab_history = window.history;
+
 		return false;
 	}
 }
